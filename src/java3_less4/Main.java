7d6f3f70b58016d6aef33b0cf55b59764ab91c1d
sevenone711 <arbitronin@yandex.ru>
2020-12-17 11:25:39 +0300

    ABC

--- src/java3_less4/Main.java
+++ src/java3_less4/Main.java
@@ -10,6 +10,58 @@ public class Main {
         static final int num = 5;
 
 
-        public static void main(String[] args) 
+        public static void main(String[] args){
+            new Thread(() -> {
+                try {
+                    for (int i = 0; i < num; i++) {
+                        synchronized (mon) {
+                            while (currentNum != 1) {
+                                mon.wait();
+                            }
+                            System.out.print("A");
+                            currentNum = 2;
+                            mon.notifyAll();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    for (int i = 0; i < num; i++) {
+                        synchronized (mon) {
+                            while (currentNum != 2) {
+                                mon.wait();
+                            }
+                            System.out.print("B");
+                            currentNum = 3;
+                            mon.notifyAll();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    for (int i = 0; i < num; i++) {
+                        synchronized (mon) {
+                            while (currentNum != 3) {
+                                mon.wait();
+                            }
+                            System.out.print("C");
+
+                            currentNum = 1;
+                            mon.notifyAll();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
     }
 
