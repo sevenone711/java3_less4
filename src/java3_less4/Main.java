a7228b26bb62847566ab05aaef999dc214bf6ab8
sevenone711 <arbitronin@yandex.ru>
2020-12-17 11:25:10 +0300

    Initial commit

---
+++ src/java3_less4/Main.java
@@ -0,0 +1,15 @@
+package java3_less4;
+
+import javax.swing.plaf.IconUIResource;
+
+public class Main {
+
+
+        static Object mon = new Object();
+        static volatile int currentNum = 1;
+        static final int num = 5;
+
+
+        public static void main(String[] args) 
+    }
+
